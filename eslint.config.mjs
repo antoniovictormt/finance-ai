75e1303263a78136cc7ad5a7060736ad6532a852
antoniovictormt <antoniovictormt@gmail.com>
2025-01-02 10:09:00 -0300

    config(project): eslint and prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,43 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      semi: "off",
+      quotes: ["error", "double"],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "prettier/prettier": "error",
+      "import-helpers/order-imports": [
+        "error",
+        {
+          newlinesBetween: "always",
+          groups: [
+            "/^next/",
+            "module",
+            "/^~/",
+            "/^@shared/",
+            ["parent", "sibling", "index"]
+          ],
+          alphabetize: { order: "asc", ignoreCase: true }
+        }
+      ]
+    }
+  })
+]
 
-export default eslintConfig;
+export default eslintConfig
